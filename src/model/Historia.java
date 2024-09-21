e23da83bb8fba777828d6fab80b11de38e4c2655
ELAINERAFAEL <162643406+ELAINERAFAEL@users.noreply.github.com>
2024-09-21 20:47:42 -0300

    Correção

---
+++ src/model/Historia.java
@@ -0,0 +1,35 @@
+package src.model;
+
+public class Historia {
+
+    private Integer id;
+    private String titulo;
+
+    public Historia() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Historia{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                '}';
+    }
+}
